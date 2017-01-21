0c5c83115fd57874c385a30f551903bded55e2ea
Kirill <alenb482@gmail.com>
2017-01-21 17:20:17 +0300

    решение задачи №5

---
+++ lang_frequency.py
@@ -0,0 +1,19 @@
+from collections import Counter
+import re
+
+def load_data(filepath):
+    with open(filepath) as file_object:
+        text = file_object.read()
+        return text
+
+def get_most_frequent_words(text):
+    word_count = Counter(x for x in re.findall(r'[A-я\']{2,}', text))
+    print(word_count.most_common(10))
+        
+
+
+if __name__ == '__main__':
+    pass
+
+
+get_most_frequent_words(load_data('123.txt'))
